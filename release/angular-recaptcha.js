826e817d7ee54d648663cfd552ebc72cb9d7530d
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2020-03-26 12:28:00 +0200

    Changed domain to the proxy

--- release/angular-recaptcha.js
+++ release/angular-recaptcha.js
@@ -1,7 +1,7 @@
 /**
- * @license angular-recaptcha build:2018-07-30
+ * @license angular-recaptcha build:2020-03-26
  * https://github.com/vividcortex/angular-recaptcha
- * Copyright (c) 2018 VividCortex
+ * Copyright (c) 2020 VividCortex
 **/
 
 /*global angular, Recaptcha */
@@ -160,7 +160,7 @@
             // Check if grecaptcha.render is not defined already.
             if (isRenderFunctionAvailable()) {
                 callback();
-            } else if ($window.document.querySelector('script[src^="https://www.google.com/recaptcha/api.js"]')) {
+            } else if ($window.document.querySelector('script[src^="https://www.recaptcha.net/recaptcha/api.js"]')) {
                 // wait for script to be loaded.
                 var intervalWait = $interval(function() {
                     if (isRenderFunctionAvailable()) {
@@ -173,7 +173,7 @@
                 var script = $window.document.createElement('script');
                 script.async = true;
                 script.defer = true;
-                script.src = 'https://www.google.com/recaptcha/api.js?onload='+provider.onLoadFunctionName+'&render=explicit';
+                script.src = 'https://www.recaptcha.net/recaptcha/api.js?onload='+provider.onLoadFunctionName+'&render=explicit';
                 $document.find('body')[0].appendChild(script);
             }
 
